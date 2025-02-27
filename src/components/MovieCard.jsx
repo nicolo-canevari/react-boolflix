cedbec15239b78aeb2142d76049e7c6d540b1533
nicolo_canevari <nico.canevari@outlook.it>
2025-02-27 17:32:46 +0100

    [FEAT] funzione per ottenere la bandiera

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,13 +2,31 @@
 
 function MovieCard({ movie, onClick }) {
 
+    // Funzione per ottenere la bandiera dalla lingua
+    function getFlagEmoji(language) {
+
+        const languageToCountry = {
+
+            it: '🇮🇹', // Italiano → Italia
+            fr: '🇫🇷', // Francese → Francia
+            es: '🇪🇸', // Spagnolo → Spagna
+            en: '🇺🇸', // Inglese → Stati Uniti
+
+        };
+
+        // Restituisce la bandiera se esiste, altrimenti mostra 🏳️
+        return languageToCountry[language] || '🏳️';
+    }
+
     return (
 
         <div className="movie-card" onClick={() => onClick(movie.id)}>
+
             <h3>{movie.title}</h3>
             <p>Titolo originale: {movie.original_title}</p>
-            <p>Lingua: {movie.original_language}</p>
+            <p>Lingua: {movie.original_language.toUpperCase()} {getFlagEmoji(movie.original_language)}</p>
             <p>Voto: {movie.vote_average}</p>
+
         </div>
 
     );
