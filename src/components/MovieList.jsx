764d2bdaace170114039e35f174129930aac6b36
nicolo_canevari <nico.canevari@outlook.it>
2025-02-28 10:51:58 +0100

    [FIX] ho adattato il css e movielist per poter stampare e visualizzare separatamente i film dalle serie tv

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,20 +3,64 @@ import MovieCard from './MovieCard';
 
 function MovieList({ movies, onSelectMovie }) {
 
+    // Divido i film dalle serie TV
+    const moviesOnly = movies.filter(movie => movie.type === 'movie');
+    const tvShowsOnly = movies.filter(movie => movie.type === 'tv');
+
     return (
 
         <div className="movie-list">
 
-            {movies.map((movie) => (
+            {moviesOnly.length > 0 && (
 
-                <MovieCard key={movie.id} movie={movie} onClick={(id, type) => onSelectMovie(id, type)} />
+                <div className="movie-section">
 
-            ))}
+                    <h2>🎬 Film</h2>
+
+                    <div className="movie-grid">
+
+                        {moviesOnly.map((movie) => (
+                            <MovieCard
+                                key={movie.id}
+                                movie={movie}
+                                onClick={(id, type) => onSelectMovie(id, type)}
+                            />
+
+                        ))}
+
+                    </div>
+
+                </div>
+
+            )}
+
+            {tvShowsOnly.length > 0 && (
+
+                <div className="movie-section">
+
+                    <h2>📺 Serie TV</h2>
+
+                    <div className="movie-grid">
+
+                        {tvShowsOnly.map((tvShow) => (
+
+                            <MovieCard
+                                key={tvShow.id}
+                                movie={tvShow}
+                                onClick={(id, type) => onSelectMovie(id, type)}
+                            />
+
+                        ))}
+
+                    </div>
+
+                </div>
+
+            )}
 
         </div>
 
     );
 
 }
-
 export default MovieList;
